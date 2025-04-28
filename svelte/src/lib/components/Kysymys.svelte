5d763029ac9a8e7b1c756da9fe05633bd82c2848
Minttu <ag4021@student.jamk.fi>
2025-04-28 10:08:23 +0300

    interface

---
+++ svelte/src/lib/components/Kysymys.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	interface Kysymys {
+		id: string;
+		img: string;
+		kysymys: string;
+		vastaukset?: string[] | null;
+		oikeaVastaus: string;
+		vaikeustaso: string;
+	}
+
+	let kysymykset: Kysymys[] = [];
+</script>
