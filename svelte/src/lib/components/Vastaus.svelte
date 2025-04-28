dbe1efbd8f139a4b4a8666d4e6ebeb707b4fd451
Minttu <ag4021@student.jamk.fi>
2025-04-28 14:04:01 +0300

    pikku juttuja

--- svelte/src/lib/components/Vastaus.svelte
+++ svelte/src/lib/components/Vastaus.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	//tämä komponentti pitäisi saada vastaukset kysymys komponentista ja muodostaa ne oikein
+	//tämä komponentti pitäisi saada vastaukset kysymys komponentista ja muodostaa ne buttoneiksi
+	//vastauksen pitäisi joko lisätä tai vähentää pisteitä ja mennä sitten eteenpäin
 	import Kysymys from './Kysymys.svelte';
+	import Button from '$lib/components/Button.svelte';
 	interface Vastaus {}
 </script>
