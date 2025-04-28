0cb80c052aa0658157da09648f37d73721d451ce
Minttu <ag4021@student.jamk.fi>
2025-04-28 13:49:34 +0300

    aloitus ja lopetussivu ja niiden välillä vaihtelu

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
+
+	let sivu: 'aloitus' | 'lopetus' = 'aloitus';
 </script>
 
 <!--Placeholdereita oikeille asioilla-->
+{#if sivu === 'aloitus'}
+	<!--Aloitussivu-->
+	<h1>404 IQ Not Found</h1>
 
-<!--Aloitussivu-->
-<h1>404 IQ Not Found</h1>
-
-<Button otsikko="Aloita" disabled={false} onclick={() => console.log('Aloitit pelin')} />
-
-<!--Lopetussivu-->
-<h1>Pelasit loppuun!</h1>
-<p>haluatko pelata uudestaan?</p>
-<Button otsikko="Uudestaan" disabled={false} onclick={() => console.log('Aloita peli uudestaan')} />
+	<Button otsikko="Aloita" disabled={false} onclick={() => (sivu = 'lopetus')} />
+{:else if sivu === 'lopetus'}
+	<!--Lopetussivu-->
+	<h1>Pelasit loppuun!</h1>
+	<p>haluatko pelata uudestaan?</p>
+	<Button otsikko="Uudestaan" disabled={false} onclick={() => (sivu = 'aloitus')} />
+{/if}
 
 <style></style>
