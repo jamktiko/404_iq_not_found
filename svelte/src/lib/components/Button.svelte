2f28d73e65b8a885df8165c69d88f39fa73c9d1a
Minttu <ag4021@student.jamk.fi>
2025-04-28 13:35:50 +0300

    button komponentti ja aloitussivu

---
+++ svelte/src/lib/components/Button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { otsikko, disabled, onclick }: { otsikko: string; disabled: boolean; onclick: () => void } =
+		$props();
+</script>
+
+<section>
+	<button {onclick} {disabled}>{otsikko}</button>
+</section>
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte';
 </script>
+
+<h1>404 IQ Not Found</h1>
+
+<Button otsikko="Aloita" disabled={false} onclick={() => console.log('Aloitit pelin')} />
+
+<style></style>
