66aae6254dd1c96ae164fd17574be1c075e6604e
Minttu <ag4021@student.jamk.fi>
2025-04-28 13:38:46 +0300

    placeholdereita

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -2,8 +2,16 @@
 	import Button from '$lib/components/Button.svelte';
 </script>
 
+<!--Placeholdereita oikeille asioilla-->
+
+<!--Aloitussivu-->
 <h1>404 IQ Not Found</h1>
 
 <Button otsikko="Aloita" disabled={false} onclick={() => console.log('Aloitit pelin')} />
+
+<!--Lopetussivu-->
+<h1>Pelasit loppuun!</h1>
+<p>haluatko pelata uudestaan?</p>
+<Button otsikko="Uudestaan" disabled={false} onclick={() => console.log('Aloita peli uudestaan')} />
 
 <style></style>
