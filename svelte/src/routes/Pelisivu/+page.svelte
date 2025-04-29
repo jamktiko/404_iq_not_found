98db84f17e70a6ca1abb1c3f6021bc64a48c087a
Minttu <ag4021@student.jamk.fi>
2025-04-29 09:32:22 +0300

    vaihdoin, että lopetussivu on samassa kuin pelisivu ja lopetussivu menee alkusivulle

--- svelte/src/routes/Pelisivu/+page.svelte
+++ svelte/src/routes/Pelisivu/+page.svelte
@@ -2,9 +2,18 @@
 	import { goto } from '$app/navigation';
 	import Button from '$lib/components/Button.svelte';
 
+	let sivu: 'peli' | 'lopetus' = 'peli';
 	//tee sivu pelille ja reititä
 </script>
 
-<h1>Pelisivu</h1>
+{#if sivu === 'peli'}
+	<!--Aloitussivu-->
+	<h1>Pelisivu</h1>
 
-<Button otsikko="takaisin" onclick={() => goto('/')} />
+	<Button otsikko="takaisin" onclick={() => (sivu = 'lopetus')} />
+{:else if sivu === 'lopetus'}
+	<!--Lopetussivu-->
+	<h1>Pelasit loppuun!</h1>
+	<p>haluatko pelata uudestaan??</p>
+	<Button otsikko="Uudestaan" disabled={false} onclick={() => goto('/')} />
+{/if}
