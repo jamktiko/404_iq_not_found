89b7dd98cefa4a8305741f23958653a2a4408531
Minttu <ag4021@student.jamk.fi>
2025-04-29 09:48:13 +0300

    pistelaskurin alku

--- svelte/src/routes/Pelisivu/+page.svelte
+++ svelte/src/routes/Pelisivu/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import Button from '$lib/components/Button.svelte';
+	import Kysymys from '$lib/components/Kysymys.svelte';
 
 	let sivu: 'peli' | 'lopetus' = $state('peli');
 
@@ -8,11 +9,16 @@
 	//Toisin sanoen auttaisi menemään läpi kaikki kysymykset
 	//Voidaan käyttää myös muihin ominaisuuksiin tarvittaessa
 	let monesKysymys: number = $state(0);
+
+	//Pistelaskuri - oma komponentti?
+	let pisteet: number = $state(0);
 </script>
 
 {#if sivu === 'peli'}
 	<!--Pelisivu-->
 	<h1>Pelisivu</h1>
+
+	<Kysymys />
 
 	<Button otsikko="takaisin" onclick={() => (sivu = 'lopetus')} />
 {:else if sivu === 'lopetus'}
