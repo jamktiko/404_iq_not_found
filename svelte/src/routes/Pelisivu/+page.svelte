c1991f39e9639c953bb05e6c851c861ac36b20f7
Minttu <ag4021@student.jamk.fi>
2025-05-02 13:30:31 +0300

    lopetussivun tyylittely

--- svelte/src/routes/Pelisivu/+page.svelte
+++ svelte/src/routes/Pelisivu/+page.svelte
@@ -152,4 +152,43 @@
 		height: 50px;
 		font-size: smaller;
 	}
+	.overlay {
+		position: relative;
+		width: 100vw;
+		height: 100vh;
+		backdrop-filter: blur(4px);
+		display: flex;
+		font-family: 'Jaro';
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		transform: translateY(15vh);
+	}
+	.center-box {
+		background-color: #000;
+		padding: 3rem;
+		border-radius: 10px;
+		border: 6px solid #ccc;
+		margin-bottom: 1rem;
+		width: 350px;
+	}
+	.center-box .viesti {
+		font-weight: bold;
+		font-size: 1.5rem;
+		padding-bottom: 40px;
+		margin-bottom: 1rem;
+		text-align: center;
+	}
+	.center-box .pisteet {
+		font-weight: bold;
+		font-size: 1.5rem;
+		text-align: center;
+	}
+
+	@media (max-width: 600px) {
+		.center-box {
+			width: 80%;
+			padding: 1.5rem;
+		}
+	}
 </style>
